75bb44b973c923812b678dd429e2f547cb8e57c7
ecthon <ecthon@gmail.com>
2022-08-06 14:42:14 -0400

    feat - componente: CardTask

---
+++ src/components/CardTask.tsx
@@ -0,0 +1,17 @@
+import { Circle, Trash } from 'phosphor-react';
+import styled from './CardTask.module.css';
+
+export function CardTask() {
+    return (
+        <li>
+            <div className={styled.card}>
+                <button><Circle color='#4EA8DE' size={18} /></button>
+                <p>
+                    Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames
+                    integer.
+                </p>
+                <button><Trash color='#808080' size={18} /></button>
+            </div>
+        </li>
+    )
+}
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,40 +1,13 @@
-import { Circle, Trash } from 'phosphor-react';
+import { CardTask } from './CardTask';
 import styled from './TaskList.module.css';
 
 export function TaskList() {
     return (
         <main className={styled.container}>
             <ul>
-                <li>
-                    <div className={styled.card}>
-                        <button><Circle color='#4EA8DE' size={18} /></button>
-                        <p>
-                            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames
-                            integer.
-                        </p>
-                        <button><Trash color='#808080' size={18} /></button>
-                    </div>
-                </li>
-                <li>
-                    <div className={styled.card}>
-                        <button><Circle color='#4EA8DE' size={18} /></button>
-                        <p>
-                            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames
-                            integer.
-                        </p>
-                        <button><Trash color='#808080' size={18} /></button>
-                    </div>
-                </li>
-                <li>
-                    <div className={styled.card}>
-                        <button><Circle color='#4EA8DE' size={18} /></button>
-                        <p>
-                            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames
-                            integer.
-                        </p>
-                        <button><Trash color='#808080' size={18} /></button>
-                    </div>
-                </li>
+                <CardTask />
+                <CardTask />
+                <CardTask />
             </ul>
         </main>
     )
