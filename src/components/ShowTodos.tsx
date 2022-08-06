c02eb54aa528af196b06ccb26ac7d80005ba14b8
ecthon <ecthon@gmail.com>
2022-08-06 11:50:07 -0400

    feat: Barra de informações de Tasks criadas/concluídas.

--- src/components/ShowTodos.tsx
+++ src/components/ShowTodos.tsx
@@ -9,7 +9,21 @@ export function ShowTodos() {
                     <input type="text" placeholder="Adicione uma nova tarefa" />
                     <button className={styled.btnAdd} >Criar <PlusCircle size={16} /></button>
                 </form>
+
             </div>
+
+            <div className={styled.contentInfoTasks}>
+                <div className={styled.infoTasks}>
+                    <span className={styled.firstSpan}>Tarefas criadas</span>
+                    <p className={styled.count}>1</p>
+                </div>
+
+                <div className={styled.infoTasks}>
+                    <span className={styled.secondSpan}>Concluídas</span>
+                    <p className={styled.count}>1</p>
+                </div>
+            </div>
+
         </main>
     )
 }
